7500ca6f70f20b38836d8379c8bf94eef6dda369
Faten Mahmoudi <faten.mahmoudi@vo2-group.com>
2023-10-10 16:07:40 +0200

    Add unit test

--- PicturesEngineAppTests/NetworkingServiceTests.swift
+++ PicturesEngineAppTests/NetworkingServiceTests.swift
@@ -5,4 +5,59 @@
 //  Created by Faten Mahmoudi on 10/10/2023.
 //
 
-import Foundation
+import XCTest
+import Combine
+
+@testable import PicturesEngineApp
+
+final class NetworkingServiceTests: XCTestCase {
+  
+  var expectation: XCTestExpectation!
+  var searchResponse: SearchResponse?
+  
+  var networking: PhotosNetworkService!
+  var error: APIError?
+  
+  var cancellables: Set<AnyCancellable>!
+  
+  override func setUpWithError() throws {
+    self.networking = PhotosNetworkLive()
+    self.cancellables = []
+  }
+  
+  override func tearDownWithError() throws {
+    self.networking = nil
+    self.cancellables = nil
+    self.error = nil
+  }
+  
+  func testGetPhotosNewtorkingService() async throws -> Void {
+    
+    var thrownError: Error?
+    let errorHandler = { thrownError = $0 }
+    let expectation = expectation(description: "get photos")
+    
+    Task {
+      do {
+        searchResponse = try await networking.getPhotos(from: "yellow flower", page: 1)
+      } catch {
+        errorHandler(error)
+      }
+      
+      expectation.fulfill()
+    }
+    
+    await waitForExpectations(timeout: 5)
+    
+    if let error = thrownError {
+      XCTFail(
+        "Async error thrown: \(error)"
+      )
+    }
+    
+    XCTAssertNotNil(self.searchResponse)
+    XCTAssertEqual(searchResponse?.photos?.count, 15)
+  }
+  
+}
+
